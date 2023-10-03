c66e398d12c9f37acf3d49159f1c1824fd7c7e18
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-03 16:39:19 +0300

    Update venus.cpp

--- venus.cpp
+++ venus.cpp
@@ -1,143 +1,168 @@
 #include <fstream>
 #include <cstring>
-#include <algorithm>
 
-#define NR_LUNI_PE_AN 12
-#define NR_ORE_PE_ZI 24
-#define MAX_CARACTERE_LUNA 20
+#define NMAX 1002
 
 using namespace std;
 
-struct comanda
-{
-	int nr, valoare, indiceExecutie;
+ifstream fin("venus.in");
+ofstream fout("venus.out");
+
+char luni[][20] = {
+	"ianuarie",
+	"februarie",
+	"martie",
+	"aprilie",
+	"mai",
+	"iunie",
+	"iulie",
+	"august",
+	"septembrie",
+	"octombrie",
+	"noiembrie",
+	"decembrie"
 };
 
-int dataInOre(int zi, char luna[], int ora);
-bool comparValoareaComenzilor(comanda a, comanda b);
+int zl[] = {
+	31, // ianuarie
+	29, // februarie
+	31, // martie
+	30, // aprilie
+	31, // mai
+	30, // iunie
+	31, // iulie
+	31, // august
+	30, // septembrie
+	31, // octombrie
+	30, // noiembrie
+	31  // decembrie
+};
+
+int sz[12];
+
+struct comanda { int v, p, nr, tc; };
+/*
+ * v = valoare
+ * p = timpul pana la care trebuie predata comanda
+ * nr = nr. de ordine al comenzii
+ * tc = timpul curent de predare al comenzii sau -1 daca nu poate fi onorat la timp
+ *
+**/
+
+int n, lg, t, pmin, L[NMAX];
+
+comanda c[NMAX], aux;
 
 int main()
 {
-	int nrComenzi, timpExecutareComanda, i;
+	int i, zi , ora, k, j, schimb;
+	char luna[20];
 
-	ifstream fin("venus.in");
+	fin >> n >> t;
 
-	fin >> nrComenzi;
-	fin >> timpExecutareComanda;
-
-	comanda comenzi[nrComenzi];
-
-	for (i = 0; i < nrComenzi; i++)
+	for (i = 1; i < 12; i++)
 	{
-		int zi, ora;
-		char luna[MAX_CARACTERE_LUNA];
-
-		fin >> comenzi[i].valoare;
-		fin >> zi >> luna >> ora;
-
-		comenzi[i].nr = i + 1;
-		comenzi[i].indiceExecutie = dataInOre(zi, luna, ora) / timpExecutareComanda;
+		sz[i] = sz[i-1] + zl[i-1];
 	}
 
-	sort(comenzi, comenzi + nrComenzi, comparValoareaComenzilor);
-
-	int comenziFinalizate[nrComenzi], comenziOmise[nrComenzi], nrComenziFinalizate, nrComenziOmise, pierdere;
-
-	nrComenziFinalizate = nrComenziOmise = pierdere = 0;
-
-	for (i = 0; i < nrComenzi; i++)
+	for (i = 1; i <= n; i++)
 	{
-		if (nrComenziFinalizate + 1 <= comenzi[i].indiceExecutie)
+		fin >> c[i].v >> zi >> luna >> ora;
+
+		for (j = 0; j < 12; j++)
 		{
-			comenziFinalizate[nrComenziFinalizate++] = comenzi[i].nr;
+			if (!strcmp(luni[j], luna)) break;
 		}
-		else
+
+		c[i].p = (sz[j] + zi-1)*24 + ora;
+		c[i].nr = i;
+	}
+
+	fin.close();
+
+	do // sortez descrescator comenzile dupa valoare
+	{
+		schimb = 0;
+
+		for (i = 1; i < n; i++)
 		{
-			comenziOmise[nrComenziOmise++] = comenzi[i].nr;
-			pierdere += comenzi[i].valoare;
+			if (c[i].v < c[i+1].v)
+			{
+				aux = c[i];
+				c[i] = c[i+1];
+				c[i+1] = aux;
+
+				schimb = 1;
+			}
+		}
+	}
+	while (schimb);
+
+	for (i = 1; i <= n && t > c[i].p; i++)
+	{
+		c[i].tc = -1;
+	}
+
+	if (i <= n)
+	{
+		L[1] = i, lg = 1;
+		c[i].tc = t;
+
+		for (i = i+1; i <= n; i++)
+		{
+			// verific daca pot onora la timp comanda c[i]
+
+			for (j = lg; j >= 1 && c[L[j]].tc + t <= c[L[j]].p && c[L[j]].p >= c[i].p; j--);
+
+			// putem insera comanda i pe pozitia j+1 ?
+
+			if (j == 0 && c[i].p >= t || c[L[j]].p <= c[i].p && c[L[j]].tc + t <= c[i].p)
+			{
+				// onorez comanda i
+
+				for (k = lg; k > j; k--)
+				{
+					c[L[k]].tc += t;
+					L[k+1] = L[k];
+				}
+
+				lg++, L[j+1] = i;
+				c[i].tc = c[L[j]].tc + t;
+			}
+			else
+			{
+				c[i].tc = -1;
+			}
 		}
 	}
 
-	ofstream fout("venus.out");
+	pmin = 0;
 
-	fout << pierdere << '\n';
-
-	for (i = 0; i < nrComenziFinalizate; i++)
+	for (i = 1; i <= n; i++)
 	{
-		fout << comenziFinalizate[i] << ' ';
+		if (c[i].tc == -1)
+		{
+			pmin += c[i].v;
+		}
 	}
 
-	for (i = 0; i < nrComenziOmise; i++)
+	fout << pmin << '\n';
+
+	for (i = 1; i <= lg; i++)
 	{
-		fout << comenziOmise[i] << ' ';
+		fout << c[L[i]].nr << ' ';
 	}
+
+	for (i = 1; i <= n; i++)
+	{
+		if (c[i].tc == -1)
+		{
+			fout << c[i].nr << ' ';
+		}
+	}
+	
+	fout.close();
 
 	return 0;
 }
-
-int dataInOre(int zi, char luna[], int ora)
-{
-	int nrZileInLuna[] = {
-		31, // ianuarie
-		29, // februarie
-		31, // martie
-		30, // aprilie
-		31, // mai
-		30, // iunie
-		31, // iulie
-		31, // august
-		30, // septembrie
-		31, // octombrie
-		30, // noiembrie
-		31 // decembrie
-	};
-
-	char luni[][MAX_CARACTERE_LUNA] = {
-		"ianuarie",
-		"februarie",
-		"martie",
-		"aprilie",
-		"mai",
-		"iunie",
-		"iulie",
-		"august",
-		"septembrie",
-		"octombrie",
-		"noiembrie",
-		"decembrie"
-	};
-
-	int indiceLuna = -1, i, ore;
-
-	for (i = 0; i < NR_LUNI_PE_AN && indiceLuna < 0; i++)
-	{
-		if (strcmp(luna, luni[i]) == 0)
-		{
-			indiceLuna = i;
-		}
-	}
-
-	for (ore = i = 0; i < indiceLuna; i++)
-	{
-		ore += nrZileInLuna[i] * NR_ORE_PE_ZI;
-	}
-	
-	return ore + (zi - 1) * NR_ORE_PE_ZI + ora;
-}
-
-bool comparValoareaComenzilor(comanda a, comanda b)
-{
-	if (a.indiceExecutie < b.indiceExecutie)
-	{
-		return true;
-	}
-
-	if (a.indiceExecutie == b.indiceExecutie
-			&& a.valoare > b.valoare)
-	{
-		return true;
-	}
-
-	return false;
-}
-// scor 50
+// scor 100 - sol oficiala
